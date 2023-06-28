4144f51676626bc5e17ae97518375d18da663733
Андрей Чернятин <chernjatn@rambler.ru>
2023-06-28 10:59:48 +0300

    fix docker-compose

--- app/Http/Middleware/AddSentCodeField.php
+++ app/Http/Middleware/AddSentCodeField.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use App\Services\Sms\NotificationService;
 use App\Services\Sms\SmsNotifiable;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Http\Request;
 use Closure;
 
@@ -13,7 +14,7 @@ class AddSentCodeField
     {
         if ($request->filled('phone')) {
             $key = NotificationService::keyGenerate(new SmsNotifiable(session()->getId(), $request->get('phone')));
-            $code = RedisManager::connection(env('REDIS_NAME', 'app_redis'))->get($key);
+            $code = Redis::connection(env('REDIS_NAME', 'app_redis'))->get($key);
             if (!is_null($code)) {
                 $request = $request->merge(['sentCode' => $code]);
             }
