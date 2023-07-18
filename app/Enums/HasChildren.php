6fbd2989df40e3013ea814a85461ce3d062738e8
Андрей Чернятин <chernjatn@rambler.ru>
2023-07-18 15:37:10 +0300

    fix

--- app/Enums/HasChildren.php
+++ app/Enums/HasChildren.php
@@ -4,9 +4,9 @@ namespace App\Enums;
 
 enum HasChildren: int
 {
-    case DEFAULT          = 1;
-    case HAS_CHILDREN     = 2;
-    case HAS_NO_CHILDREN  = 3;
+    case DEFAULT         = 1;
+    case HAS_CHILDREN    = 2;
+    case HAS_NO_CHILDREN = 3;
 
 
 public function valueForManzana(): int
